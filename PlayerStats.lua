433fff9db2ff74b075974e971f75def145fbc24f
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-03 00:41:56 -0700

    Update PlayerStats.lua

--- PlayerStats.lua
+++ PlayerStats.lua
@@ -1,43 +1,24 @@
---[[
-	WARNING: Do NOT test this script the game in Roblox Studios
-	or it will overwrite another player's data. It will also not
-	work if you're executing this script in RobloxStudios. You
-	will need to publish the game and play from the website.
-
-	This script utilizes data writing, data reading, and data
-	recovery. This API is called DataStore2. It 100% guarantees
-	data loss prevention. DataStore is ineffective in recovering
-	data since all player's stats can get reset to its default
-	values.
-	
-	Credits go to the creator of this API, Kampfkarren. You can
-	look up DataStore2 library:
-	https://devforum.roblox.com/t/how-to-use-datastore2-data-store-caching-and-data-loss-prevention/136317
-
-	INSTRUCTION: Place this script in "ServerScriptService"
-]]--
-
 --Gets DataStore2 module
 local DataStore2 = require(1936396537)
 
 --If the player has never played the game, then give them this
 local default_level = 1
-local default_kills = 0
+local default_cash = 0
 local default_xp = 0
 
 --Combines three keys (level, kills, and xp)
-DataStore2.Combine("DATA", "levelData", "killsData", "XP_Data")
+DataStore2.Combine("DATA", "level", "cash", "XP")
 
 game.Players.PlayerAdded:Connect(function(player)
 	
 	--Player's key for level, kills, and xp
-	local level_DataStore = DataStore2("levelData", player)
-	local kills_DataStore = DataStore2("killsData", player)
-	local XP_DataStore = DataStore2("XP_Data", player)
+	local level_DataStore = DataStore2("level", player)
+	local cash_DataStore = DataStore2("cash", player)
+	local XP_DataStore = DataStore2("XP", player)
 	
 	--Sets backups for each storage in case data loss occurs
 	level_DataStore:SetBackup(10)
-	kills_DataStore:SetBackup(10)
+	cash_DataStore:SetBackup(10)
 	XP_DataStore:SetBackup(10)
 
 	--Clears level backup if it's set
@@ -46,8 +27,8 @@ game.Players.PlayerAdded:Connect(function(player)
 	end
 	
 	--Clears kills backup if it's set
-	if kills_DataStore:IsBackup() then
-		kills_DataStore:ClearBackup()
+	if cash_DataStore:IsBackup() then
+		cash_DataStore:ClearBackup()
 	end
 	
 	
@@ -61,54 +42,40 @@ game.Players.PlayerAdded:Connect(function(player)
 	leaderstats.Name = "leaderstats"
 	
 	--Player's level
-	local level = Instance.new("IntValue", leaderstats)
-	level.Name = "Level"
-	
-	--Player's kills
-	local kills = Instance.new("IntValue", leaderstats)
-	kills.Name = "Kills"
+	local LEVEL = Instance.new("IntValue", leaderstats)
+	LEVEL.Name = "Level"
 	
 	--Player's XP
-	local xp = Instance.new("IntValue", leaderstats)
-	xp.Name = "XP"
+	local XPoints = Instance.new("IntValue", leaderstats)
+	XPoints.Name = "XP"
+
+	--Player's cash
+	local CASH = Instance.new("IntValue", leaderstats)
+	CASH.Name = "Cash"
 		
 	--Calls everytime the level gets updated
 	local function updateLevel(updateLevelData)
-		level.Value = level_DataStore:Get(updateLevelData)
+		LEVEL.Value = level_DataStore:Get(updateLevelData)
 	end
 	
 	--Calls everytime kill counts gets updated
-	local function updateKills(updateKillsData)
-		kills.Value = kills_DataStore:Get(updateKillsData)
+	local function updateCash(updateCashData)
+		CASH.Value = cash_DataStore:Get(updateCashData)
 	end
 		
 	--Calls everytime XP gets updated
 	local function updateXP(updateXPData)
-		xp.Value = XP_DataStore:Get(updateXPData)
+		XPoints.Value = XP_DataStore:Get(updateXPData)
 	end
 	
 	--[[Functions will be called once, because the player 
 		has not been in this game before ]]
 	updateLevel(default_level)
-	updateKills(default_kills)
+	updateCash(default_cash)
 	updateXP(default_xp)
 
-	--Callback functions that get called whenever the player's data changed
+	--Callback functions that get called to update player's data
 	level_DataStore:OnUpdate(updateLevel)
-	kills_DataStore:OnUpdate(updateKills)
+	cash_DataStore:OnUpdate(updateCash)
 	XP_DataStore:OnUpdate(updateXP)
-	
-	--When player joins, then generate appropriate max HP
-	local init_humanoid = player.Character:WaitForChild("Humanoid")
-	init_humanoid.MaxHealth = 100 + (50 *(level.Value - 1))
-	init_humanoid.Health = init_humanoid.MaxHealth
-	
-	--Generates player's health whenever they respawn
-	player.CharacterAdded:connect(function(char)
-		local humanoid = char:WaitForChild("Humanoid")
-		if humanoid then
-			humanoid.MaxHealth = 100 + (50 *(level.Value - 1))
-			humanoid.Health = humanoid.MaxHealth
-		end
-	end)
 end)
