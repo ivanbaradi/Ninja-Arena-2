a13147b6ca5409ea994cd93939f9d723287f2524
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-03 01:03:20 -0700

    Update PlayerLevel.lua

--- PlayerGUI/PlayerLevel.lua
+++ PlayerGUI/PlayerLevel.lua
@@ -2,16 +2,7 @@
 local player = game.Players.LocalPlayer
 --Gets Player's Current Level
 local player_level = player.leaderstats.Level
---Old Level Value
-local old_level = player_level.Value
---Initially displays player's current level
-script.Parent.Text = "Level "..player_level.Value
 
 while wait(0.1) do
-	--Will need to update the text to display the correct level of the player
-	if old_level ~= player_level.Value then
-		old_level = player_level.Value
-		script.Parent.Text = "Level "..player_level.Value
-		print("Player Level Up!")
-	end
+	script.Parent.Text = "Level "..player_level.Value
 end
