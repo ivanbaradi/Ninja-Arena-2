889ea97499cf2174994e7213b964a1e363cfdf61
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-06 00:58:57 -0700

    Update PlayerXP.lua

--- PlayerGUI/PlayerXP.lua
+++ PlayerGUI/PlayerXP.lua
@@ -2,8 +2,18 @@
 local player = game.Players.LocalPlayer
 --Gets Player's Current XP
 local player_XP = player.leaderstats.XP
+--Target XP Multiplier
+local XP_Multiplier
 
---Displays player's current XP and target XP
 while wait(0.1) do
-	script.Parent.Text = player_XP.Value.."/"..(player.leaderstats.Level.Value * 1000).."XP"
+	
+	--Player gets higher XP multiplier as they level up
+	if player.leaderstats.Level.Value < 9 then
+		XP_Multiplier = 500
+	else
+		XP_Multiplier = 750
+	end
+	
+	--Displays player's current XP and target XP
+	script.Parent.Text = player_XP.Value.."/"..(player.leaderstats.Level.Value * XP_Multiplier).."XP"
 end
