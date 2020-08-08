7c9a934688e2f2e6722f5773ab771894aacb52e7
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-07 19:21:06 -0700

    Update DisplayTeleportMessage.lua

--- TeleportToPlaceGUI/DisplayTeleportMessage.lua
+++ TeleportToPlaceGUI/DisplayTeleportMessage.lua
@@ -1,11 +1,16 @@
 --SendPlayerToPlace Remote Event 
 sendPlayerToPlace = game.ReplicatedStorage:WaitForChild("SendPlayerToPlace")
 
+--True, if the message is sent (Prevents the message from reconcatenation if the user rapidly clicks the button)
+message_sent = false
+
 --Opens Teleport Message to Player's Screen
 sendPlayerToPlace.OnClientEvent:Connect(function(game_mode)
-	
-	--Tells player to where they are being teleported to
-	script.Parent.TextLabel.Text = script.Parent.TextLabel.Text..game_mode
-	--Makes Teleport Message visible to the player
-	script.Parent.Visible = true
+	if not message_sent then
+		--Tells player to where they are being teleported to
+		script.Parent.TextLabel.Text = script.Parent.TextLabel.Text..game_mode
+		--Makes Teleport Message visible to the player
+		script.Parent.Visible = true
+		message_sent = true
+	end
 end)
