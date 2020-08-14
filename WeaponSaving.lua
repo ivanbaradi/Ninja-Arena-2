db832d40f5e2a982b39b4e6070a09ae0644da6a1
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-14 13:20:45 -0700

    Update WeaponSaving.lua

--- WeaponSaving.lua
+++ WeaponSaving.lua
@@ -1,15 +1,17 @@
+--Will need to use DataStoreService API to render saving and loading player weapons
 local DataStore = game:GetService("DataStoreService")
 local BackpackStore = DataStore:GetDataStore("WeaponSave")
 
---Loading player weapon's
+--Loads player's weapons when they join the server
 game.Players.PlayerAdded:Connect(function(player)
 	
+	--Loads player's inventory
 	local player_weapons = BackpackStore:GetAsync("User-"..player.UserId)
 	
+	--Will go through player's weapons to be replicated into their backpacks and startergears
 	print("Weapons to load:")
 	for i, weapon in pairs(player_weapons) do
 		local loading_weapon = game.ReplicatedStorage:FindFirstChild(weapon)
-		
 		if loading_weapon then
 			loading_weapon:Clone().Parent = player.Backpack
 			loading_weapon:Clone().Parent = player.StarterGear
@@ -20,18 +22,17 @@ game.Players.PlayerAdded:Connect(function(player)
 	end
 end)
 
+--Saves player's weapons when they leave the server
 game.Players.PlayerRemoving:Connect(function(player)
 	
+	--Will need to create a table to save all weapons from the starter gear
 	local player_backpack = {}
 	
-	--Adds all player weapons besides the Wooden Sword in the starter gear 
+	--Adds all player weapons in the starter gear 
 	for i, weapon in pairs(player.StarterGear:GetChildren()) do
 		if weapon then
-			print(weapon.Name)
-			--if weapon.Name ~= "Wooden Sword" then
 			table.insert(player_backpack, i, weapon.Name)
 			print(weapon.Name.." is inserted in the table")
-			--end
 		end
 	end
 			
@@ -43,5 +44,6 @@ game.Players.PlayerRemoving:Connect(function(player)
 		end
 	end
 	
+	--Saves player's inventory
 	BackpackStore:SetAsync("User-"..player.UserId, player_backpack)
 end)
