67cd8b26e8142e289773952d2b7a455f0dbe3497
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-14 13:06:40 -0700

    Update GiveWeaponToPlayer.lua

--- ShopGUI/GiveWeaponToPlayer.lua
+++ ShopGUI/GiveWeaponToPlayer.lua
@@ -1,19 +1,11 @@
---[[
-
-This script will replicate a new weapon to the client's backpack 
-from ReplicateStorage after the client purchased it. It will 
-also save the backpack data, which includes all weapons that the
-client bought. Next time the client joins the server, they will
-retrieve all weapons.
-
-]]
-
 --DataStore 2
 local DataStore2 = require(1936396537)
-DataStore2.Combine("DATA", "level", "cash", "XP", "backpack")
+DataStore2.Combine("DATA", "level", "cash", "XP")
 
 --Used to have server communicate with the client in order to replicate weapon
 give_weapon = game.ReplicatedStorage:FindFirstChild("GiveWeapon")
+--RestockWeapons Event
+restockWeapons = game.ReplicatedStorage:FindFirstChild("RestockWeapons")
 
 --Called when players buy a weapon
 give_weapon.OnServerEvent:Connect(function(player, new_weapon, subtract_cash)
@@ -26,8 +18,9 @@ give_weapon.OnServerEvent:Connect(function(player, new_weapon, subtract_cash)
 		
 		--Gets current cash value for player
 		local cash_DataStore = DataStore2("cash", player)
+		
 		--Gets player's old backpack
-		local backpack_DataStore = DataStore2("backpack", player)
+		--local backpack_DataStore = DataStore2("backpack2", player)
 		
 		--Takes cash from player for a weapon
 		player.leaderstats.Cash.Value = player.leaderstats.Cash.Value - subtract_cash
@@ -38,23 +31,36 @@ give_weapon.OnServerEvent:Connect(function(player, new_weapon, subtract_cash)
 		weaponBP.Parent = player.Backpack 
 		weaponSG.Parent = player.StarterGear
 		
-		--Array containing all bought weapons plus the new one
-		local player_backpack = {}
+		--Saves the weapon item in the backpack (Code this part)
+		--local player_backpack = {}
 		
-		--Adds all player weapons besides the Wooden Sword in the backpack 
-		for i, weapon in pairs(player.Backpack:GetChildren()) do
-			print(weapon.Name)
-			if weapon.Name ~= "Wooden Sword" then
-				table.insert(player_backpack, i-1, weapon.Name)
-				print(weapon.Name.." is inserted in the table")
+		--Adds all player weapons besides the Wooden Sword in the starter gear 
+		--[[for i, weapon in pairs(player.Backpack:GetChildren()) do
+			if weapon then
+				print(weapon.Name)
+				if weapon.Name ~= "Wooden Sword" then
+					table.insert(player_backpack, i-1, weapon.Name)
+					print(weapon.Name.." is inserted in the table")
+				end
 			end
 		end
+				
+		--Prints all weapons
+		print("Player weapons to be saved:")
+		for j, print_weapon in pairs(player_backpack) do
+			if print_weapon then
+				print("Weapon "..j..": "..print_weapon)
+			end
+		end]]
 		
 		--Saves all weapons (including the new one)
-		backpack_DataStore:Set(player_backpack)
+		--backpack_DataStore:Set(player_backpack)
 		
 		--Saves cash value (Note: This often works if you don't test it in Roblox Studios)
 		cash_DataStore:Set(player.leaderstats.Cash.Value)
+		
+		--Will need to restock weapons at Sell UI everytime a player purchases a weapon
+		restockWeapons:FireClient(player)
 	else
 		print("You already have that weapon")
 	end
