1651fde4696267757318cd9d9e48bd10f1c2f078
Ivan Baradi <37347359+NinjaGodIvan@users.noreply.github.com>
2020-08-31 22:57:08 -0700

    Update WeaponSaving.lua

--- WeaponSaving.lua
+++ WeaponSaving.lua
@@ -1,49 +1,46 @@
---Will need to use DataStoreService API to render saving and loading player weapons
+--[[ This script saves all player's weapons when the player leaves and loads them when 
+the player joins the game. It should not load the gamepass weapons.]]
+
 local DataStore = game:GetService("DataStoreService")
 local BackpackStore = DataStore:GetDataStore("WeaponSave")
 
---Loads player's weapons when they join the server
+--Loading player's weapons (No Gamepass Weapons are to be loaded)
 game.Players.PlayerAdded:Connect(function(player)
 	
-	--Loads player's inventory
 	local player_weapons = BackpackStore:GetAsync("User-"..player.UserId)
 	
-	--Will go through player's weapons to be replicated into their backpacks and startergears
 	print("Weapons to load:")
 	for i, weapon in pairs(player_weapons) do
 		local loading_weapon = game.ReplicatedStorage:FindFirstChild(weapon)
 		if loading_weapon then
-			loading_weapon:Clone().Parent = player.Backpack
-			loading_weapon:Clone().Parent = player.StarterGear
-			print(loading_weapon)
+			if weapon.Name ~= "Bluesteel Katana" and weapon.Name ~= "Immortal God Long Sword" then
+				loading_weapon:Clone().Parent = player.Backpack
+				loading_weapon:Clone().Parent = player.StarterGear
+				print(loading_weapon)
+			end
 		else
 			print(weapon.." no longer exists in ReplicateStorage")
 		end
 	end
 end)
 
---Saves player's weapons when they leave the server
+--Saving player's weapons (No Gamepass Weapons are to be saved)
 game.Players.PlayerRemoving:Connect(function(player)
 	
-	--Will need to create a table to save all weapons from the starter gear
 	local player_backpack = {}
+	local count = 1
 	
-	--Adds all player weapons in the starter gear 
+	--Adds all player weapons besides the Wooden Sword in the starter gear 
+	print("Weapons to be saved:")
 	for i, weapon in pairs(player.StarterGear:GetChildren()) do
 		if weapon then
-			table.insert(player_backpack, i, weapon.Name)
-			print(weapon.Name.." is inserted in the table")
-		end
-	end
-			
-	--Prints all weapons
-	print("Player weapons to be saved:")
-	for j, print_weapon in pairs(player_backpack) do
-		if print_weapon then
-			print("Weapon "..j..": "..print_weapon)
+			if weapon.Name ~= "Bluesteel Katana" and weapon.Name ~= "Immortal God Long Sword" then
+				table.insert(player_backpack, count, weapon.Name)
+				print("Weapon "..count..": "..weapon.Name)
+				count = count + 1
+			end
 		end
 	end
 	
-	--Saves player's inventory
 	BackpackStore:SetAsync("User-"..player.UserId, player_backpack)
 end)
