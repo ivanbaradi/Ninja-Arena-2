51a8c5a8c11d6218b056cc808ade0ebbb863294d
Ivan Baradi <ivan4ubc@yahoo.com>
2021-03-30 11:50:43 -0700

    files edited

--- Main/Gameplay/Server/UpdatePlayerStats.lua
+++ Main/Gameplay/Server/UpdatePlayerStats.lua
@@ -4,39 +4,23 @@ DataStore2.Combine("DATA", "level", "cash", "XP")
 --MarketPlace
 local MarketPlace = game:GetService("MarketplaceService")
 
---Gives player cash
-function giveCash(player, leaderstats, cash)
-	
+--Updates player stats
+function updateStats(player, player_stats, update_val, store_type)
 	--Will check to see if you have VIP to earn double or triple cash
 	if MarketPlace:UserOwnsGamePassAsync(player.UserId, 11420646) --[[MEGA VIP 3x Cash]] then
-		leaderstats.Cash.Value += (cash * 3)
+		player_stats += (update_val * 3)
 	elseif MarketPlace:UserOwnsGamePassAsync(player.UserId, 11420620) --[[VIP 2x Cash]] then
-		leaderstats.Cash.Value += (cash * 2)
+		player_stats += (update_val * 2)
 	else
-		leaderstats.Cash.Value += cash
+		player_stats += update_val
 	end
 
 	--Saves Player's Cash
-	DataStore2("cash", player):Set(leaderstats.Cash.Value)
-	print(player.Name.." earned cash!")
+	DataStore2(store_type, player):Set(player_stats)
 end
 
---Gives player XP
-function giveXP(player, leaderstats, XP)
-
-	--Player earns XP and check to see if you have VIP to earn double or triple XP
-	if MarketPlace:UserOwnsGamePassAsync(player.UserId, 11420646) --[[MEGA VIP 3x XP]] then
-		leaderstats.XP.Value += (XP * 3)
-	elseif MarketPlace:UserOwnsGamePassAsync(player.UserId, 11420620) --[[VIP 2x XP]] then
-		leaderstats.XP.Value += (XP * 2)
-	else
-		leaderstats.XP.Value += XP
-	end
-	
-	--Saves Player's XP
-	DataStore2("XP", player):Set(leaderstats.XP.Value)
-
-	--Levels up player if they reach target XP, and regenerates player's health 
+--Levels up player if they reach target XP, and regenerates player's health 
+function updateLevel(player, leaderstats)
 	if leaderstats.XP.Value >= player:FindFirstChild("Target XP").Value then
 
 		--Levels up player
@@ -48,19 +32,22 @@ function giveXP(player, leaderstats, XP)
 
 		--Regenerates player's health after leveling up
 		game.ServerStorage:FindFirstChild("Health Regeneration"):Fire(player, player.Character:FindFirstChildOfClass("Humanoid"), leaderstats)
+	else
+		print(player.Name.." cannot level up more than once")
 	end
 end
 
---Updates players new stats (level, cash, and xp)
+--Updates player new stats (level, cash, and xp)
 game.ServerStorage:FindFirstChild("Send Player New Stats").Event:Connect(function(player, leaderstats, cash, XP)
 	if leaderstats then					
 		
 		--Gives players cash
-		giveCash(player, leaderstats, cash)
+		updateStats(player, leaderstats.Cash.Value, cash, "cash")
 
 		--Players must be under Level 20 to earn XP and level up
 		if leaderstats.Level.Value < 20 then
-			giveXP(player, leaderstats , XP)
+			updateStats(player, leaderstats.XP.Value, XP, "XP")
+			updateLevel(player, leaderstats)
 		else
 			print("You have reached max level to earn XP here.")
 		end
